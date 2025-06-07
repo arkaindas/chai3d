28da6316521a30949f6cf87e0b992166b2ffc6b3
arkaindas <arka263249@gmail.com>
2025-06-07 23:22:21 +0530

    trst

--- src/ChaiContainer.jsx
+++ src/ChaiContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useState,Suspense } from 'react';
 import {Canvas,useLoader,useFrame} from "@react-three/fiber"
-import { XR, createXRStore } from '@react-three/xr'
+import { XR, XROrigin, createXRStore } from '@react-three/xr'
 //import Cube from './cube';
 import logo from './assets/react.svg'
 import {TextureLoader} from 'three'
@@ -8,7 +8,7 @@ import * as THREE from 'three'
 import Model from "./Chai"; 
 import { OrbitControls,Environment,Stage } from "@react-three/drei";
 
-const store = createXRStore();
+const store = createXRStore({depthSensing:true,hand:false});
 
 export default function ChaiContainer() {
 
@@ -20,14 +20,19 @@ export default function ChaiContainer() {
                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                       </svg>
         </a>
-        <Canvas camera={{ position: [0, 0, -0.2], near: 0.025 }}>
-            <XR store={store}>
-            <OrbitControls />
+        <Canvas camera={{ position: [0, 0, -0.2], near: 0.025,fov:35 }}>
+        <XR store={store}>
+            <OrbitControls minPolarAngle={Math.PI / 2.2} maxPolarAngle={Math.PI/2.2} />
             <ambientLight intensity={0.5} />
             <Stage environment="city">
             {/* <directionalLight position={[10, 10, 5]} intensity={1} />
             <spotLight position={[5, 5, 5]} intensity={1} angle={0.3} penumbra={0.2} /> */}
-            <Model />
+            <Suspense>
+            <Model scale={0.5} />
+            </Suspense>
+            <group position={[0,0,5.6]}>
+                <XROrigin/>
+            </group>
             {/* <primitive object={gltf.scene} /> */}
             </Stage>
             </XR>
